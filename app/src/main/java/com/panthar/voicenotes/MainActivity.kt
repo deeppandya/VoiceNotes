92c6e28f1bb932b810d1e6144585a8a0cac72820
DEEP_PC\deepp <deeppandya91@gmail.com>
2025-05-10 17:44:16 -0400

    Add UI changes for recording state with live transcription

--- app/src/main/java/com/panthar/voicenotes/MainActivity.kt
+++ app/src/main/java/com/panthar/voicenotes/MainActivity.kt
@@ -4,28 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.panthar.voicenotes.composables.NavigationTopBar
 import com.panthar.voicenotes.ui.theme.VoiceNotesTheme
 
@@ -54,35 +62,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Homepage(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
-        SmallFloatingActionButton(
-            onClick = {  },
-            shape = CircleShape,
-            modifier = Modifier.size(40.dp),
-            containerColor = Color.LightGray,
-            contentColor = Color.White
+    val context = LocalContext.current
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-            Icon(Icons.Filled.PlayArrow, "Large floating action button")
+            Box(modifier = Modifier
+                .size(12.dp)
+                .clip(CircleShape)
+                .background(Color.Red))
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(context.getString(R.string.recording_on))
         }
-        Spacer(modifier = Modifier.width(8.dp))
-        FloatingActionButton(
-            onClick = {  },
-            shape = CircleShape,
-            modifier = Modifier.size(80.dp),
-            containerColor = Color.White,
-            contentColor = Color.LightGray
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clip(
+                    RoundedCornerShape(12.dp)
+                )
+                .background(Color.White)
+                .height(100.dp)
         ) {
-            Icon(Icons.Filled.Home, "Large floating action button")
+            Text(text = "Large floating action button", modifier = Modifier.padding(8.dp))
         }
-        Spacer(modifier = Modifier.width(8.dp))
-        SmallFloatingActionButton(
-            onClick = {  },
-            shape = CircleShape,
-            modifier = Modifier.size(40.dp),
-            containerColor = Color.LightGray,
-            contentColor = Color.White
+        Spacer(modifier = Modifier.height(32.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-            Icon(Icons.Filled.Delete, "Large floating action button")
+            SmallFloatingActionButton(
+                onClick = { },
+                shape = CircleShape,
+                modifier = Modifier.size(40.dp),
+                containerColor = Color.LightGray,
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Filled.PlayArrow, "Large floating action button")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            FloatingActionButton(
+                onClick = { },
+                shape = CircleShape,
+                modifier = Modifier.size(80.dp),
+                containerColor = Color.White,
+                contentColor = Color.LightGray
+            ) {
+                Icon(rememberAsyncImagePainter(R.drawable.ic_baseline_mic_24), "Large floating action button")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            SmallFloatingActionButton(
+                onClick = { },
+                shape = CircleShape,
+                modifier = Modifier.size(40.dp),
+                containerColor = Color.LightGray,
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Filled.Delete, "Large floating action button")
+            }
         }
     }
 }
