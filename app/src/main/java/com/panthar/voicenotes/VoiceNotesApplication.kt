78676d2ff16f8c788ec81289be57ae22c5b241f1
DEEP_PC\deepp <deeppandya91@gmail.com>
2025-05-13 05:08:20 -0400

    Stop VoiceNotes overlay service nad hide bubble button if application is in foreground

--- app/src/main/java/com/panthar/voicenotes/VoiceNotesApplication.kt
+++ app/src/main/java/com/panthar/voicenotes/VoiceNotesApplication.kt
@@ -1,7 +1,39 @@
 package com.panthar.voicenotes
 
 import android.app.Application
+import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import com.panthar.voicenotes.service.VoiceNotesOverlayService
 import dagger.hilt.android.HiltAndroidApp
+import androidx.lifecycle.ProcessLifecycleOwner
 
 @HiltAndroidApp
-class VoiceNotesApplication : Application()
+class VoiceNotesApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Add the lifecycle observer to listen for app state changes
+        ProcessLifecycleOwner.get().lifecycle.addObserver(AppLifecycleObserver(this))
+    }
+}
+
+class AppLifecycleObserver(private val context: Context) : DefaultLifecycleObserver {
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        // App is in the foreground, stop the service if running
+        stopService()
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+        // App is in the background, you can choose to leave the service running or not
+    }
+
+    private fun stopService() {
+        val serviceIntent = Intent(context, VoiceNotesOverlayService::class.java)
+        context.stopService(serviceIntent)
+    }
+}
